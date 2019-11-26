60e298d941267fc8c0a25402d91e9788c508a8ec
Stefan Bratic <cobrijani@gmail.com>
2019-11-26 16:54:21 +0100

    Initiated demo project

---
+++ demo/app/src/main/java/com/github/cobrijani/pf4jspringbootstarterdemoapp/Pf4jSpringBootStarterDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.cobrijani.pf4jspringbootstarterdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pf4jSpringBootStarterDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pf4jSpringBootStarterDemoAppApplication.class, args);
+	}
+
+}
---
+++ demo/app/src/test/java/com/github/cobrijani/pf4jspringbootstarterdemoapp/Pf4jSpringBootStarterDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cobrijani.pf4jspringbootstarterdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pf4jSpringBootStarterDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
